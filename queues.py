d518b09253c7edda5f78a1ca84f1d8187ebac604
mehul-tuteck <107098992+mehul-tuteck@users.noreply.github.com>
2023-07-15 15:49:54 +0530

    Implemented custom queues

---
+++ queues.py
@@ -0,0 +1,28 @@
+class Queue:
+
+    def __init__(self) -> None:
+        self.queue = []
+
+    def enqueue(self,value):
+        self.queue.append(value)
+    
+    def dequeue(self):
+        if self.isEmpty():
+            return "There are no elements in the queue to be dequeued!"
+        element_to_be_popped = self.queue[0]
+        self.queue = self.queue[1:]
+        return element_to_be_popped
+    
+    def isEmpty(self) -> bool:
+        return self.queue.size() <= 0 
+
+    def size(self) -> int:
+        return len(self.queue)
+    
+    def peek(self):
+        if self.isEmpty():
+            return "There are no elements in the queue to be peeked!"
+        return self.queue[0]
+        
+        
+
